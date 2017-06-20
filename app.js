5f628576fb0b1570266239e1bf91d2b2105a9cf3
Marc Wustrack <mac@basecom.de>
2017-06-20 13:11:39 +0200

    Added Cache and web output

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-var market = require('steam-market-pricing');
+const market = require('steam-market-pricing');
+const http = require('http')
+const NodeCache = require( "node-cache" );
+const myCache = new NodeCache( { stdTTL: 60, checkperiod: 60 } );
+const port = 7331
 
+const requestHandler = (request, response) => {
+  if(request.url == "/") {
+    getAPIData(request, response);
+  }
+}
 
-getAPIData();
-setInterval(getAPIData, 60 * 1000);
+const server = http.createServer(requestHandler)
 
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  console.log("server is running");
+});
 
-function getAPIData() {
+function getAPIData(request, response) {
   var names = [
       'Grey Shirt',
       'Red Hi-top Trainers',
@@ -22,18 +36,33 @@ function getAPIData() {
     360
   ];
   market.getItemsPrice(578080, names, function(data) {
+    response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+    var cacheValue = myCache.get("STEAM_API");
+    if(cacheValue != undefined) {
+      console.log("CACHE HIT");
+      response.end(cacheValue);
+      return;
+    }
+
     var ausgangsSumme = 0;
     var currentSumme = 0;
+    var responseString = "";
+
     for(var i in names) {
-        console.log(names[i] + ' lowest price: ' + data[names[i]]['lowest_price'] + ", ausgangsPreis: " + ausgangsPreis[i] + "€");
+        responseString = responseString + names[i] + ' lowest price: ' + data[names[i]]['lowest_price'] + ", ausgangsPreis: " + ausgangsPreis[i] + "€<br/>";
         ausgangsSumme = ausgangsSumme + ausgangsPreis[i];
         currentSumme = currentSumme + parseFloat(data[names[i]]['lowest_price']);
     }
-    console.log("");
-    console.log("");
-    console.log("Ausgang: " + parseFloat(ausgangsSumme).toFixed(2)+ "€");
-    console.log("Aktuell: " + parseFloat(currentSumme).toFixed(2)+ "€");
-    console.log("");
-    console.log("");
+    responseString = responseString + "<br/>";
+    responseString = responseString + "<br/>";
+    responseString = responseString + "Ausgang: " + parseFloat(ausgangsSumme).toFixed(2)+ "€<br/>";
+    responseString = responseString + "Aktuell: " + parseFloat(currentSumme).toFixed(2)+ "€<br/>";
+    responseString = responseString + "<br/>";
+    responseString = responseString + "<br/>";
+
+    myCache.set("STEAM_API", responseString, 60);
+    console.log("CACHE MISS");
+
+    response.end(responseString);
   }, 3);
 }
